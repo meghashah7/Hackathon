aa7b1fcde08eb0fa725c63703f6e3bd390b7d9dc
meghashah7 <43990397+meghashah7@users.noreply.github.com>
2019-06-18 17:46:56 +0530

    Convert string to integer
    Implement atoi which converts a string to an integer.
    
    The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
    
    The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
    
    If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
    
    If no valid conversion could be performed, a zero value is returned.

---
+++ Atoi.go
@@ -0,0 +1,53 @@
+import "strings"
+
+func myAtoi(str string) int {
+   	INT_MAX := 2147483647
+	INT_MIN := -2147483648
+
+	if str == "" {
+		return 0
+	}
+	//a := regexp.MustCompile(`0123456789.`)
+	var word string
+	str1 := strings.Split(str, " ")
+	for i := range str1 {
+		if str1[i] != "" {
+			word = str1[i]
+			break
+		}
+	}
+	//fmt.Println(word)
+	//j := 0
+	var str2 []byte
+	for i := range word {
+		if (word[i] == '+' || word[i] == '-') && i == 0 {
+			str2 = append(str2, word[i])
+
+		} else {
+	//		fmt.Println(rune(word[i]))
+			if (word[i] >= '0' && word[i] <= '9') || word[i] == '.' {
+				str2 = append(str2, word[i])
+
+			} else {
+				break
+			}
+
+		}
+	}
+	//fmt.Println(string(str2))
+	str3 := string(str2[:])
+	//fmt.Println(str3)
+	str4 := strings.Split(str3, ".")
+	//fmt.Println(str4)
+	number, _ := strconv.Atoi(str4[0])
+	//fmt.Println(number)
+	
+		if number > INT_MAX {
+			return INT_MAX
+		} else if number < INT_MIN {
+			return INT_MIN
+		} else {
+			return number
+		}
+	
+}
